37682471a2846ee478389385f047bd42aacf270c
Kevin Shen <kevin_shen@hotmail.ca>
2018-02-08 04:02:24 -0500

    Added js lecture

---
+++ Lec7/js demo/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  "use strict";
+
+  function myFunction() {
+
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('http://www.utsc.utoronto.ca/cms/sites/utsc.utoronto.ca.cms/files/styles/person-thumbnail/public/Nick_Cheng.jpg?itok=p_yqXXPh')";
+  }
+};
